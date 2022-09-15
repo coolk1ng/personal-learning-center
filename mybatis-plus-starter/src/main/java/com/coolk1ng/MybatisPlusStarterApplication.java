c012487cb41c4e1122fd4c29284cf98ee6ba88c7
coolk1ing <codesniperrrrr@gmail.com>
2022-09-16 01:43:46 +0800

    mybatis-plus-starter

---
+++ mybatis-plus-starter/src/main/java/com/coolk1ng/MybatisPlusStarterApplication.java
@@ -0,0 +1,13 @@
+package com.coolk1ng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusStarterApplication.class, args);
+    }
+
+}
